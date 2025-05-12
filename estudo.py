22a41fe3a4e68367b01320baafedaa69bee17e6c
nenelxd <myd.artcore@gmail.com>
2025-05-12 14:44:35 -0300

    Add files via upload

---
+++ estudo.py
@@ -0,0 +1,80 @@
+# def saudacao(nome):
+#     mensagem = "Olá, " + nome
+#     print(mensagem)
+
+# saudacao("Lucas")
+
+# def calcular_media(n1, n2, n3):
+#     media = (n1 + n2 + n3) / 3
+#     print(f"A média é: {media:.0f}")
+
+# calcular_media(7, 8, 9)
+
+
+# def verificar_par_impar(numero):
+#     if numero %2 == 0:
+#         print("Número par")
+#     else:
+#         print("Número impar")
+
+# verificar_par_impar(7)
+
+# def contar_ate(n):
+#     i = 1
+#     while n <= i:
+#         i + 1
+#         print(i)
+
+# contar_ate(5)
+
+# contar = 1
+
+# while contar <= 10:
+#     print(contar)
+#     contar += 1
+
+
+# total = 0
+# num = 1
+
+# while num <= 100:
+#     total += num
+#     num += 1
+# print(f"A soma dos números de 1 a 100 é: {total}")
+
+# numero = int(input("Digite um número: "))
+# total = 0
+
+# while total <= numero * 10:
+#     print (total)
+#     total += numero
+
+# i = 10
+
+# while i >= 0:
+#     print(i)
+#     i -= 1
+# print("Feliz Ano Novo")
+
+
+# numero = int(input("Insira um número: "))
+# total = 1
+
+# while total != numero:
+#     if total %2 != 0:
+#         print(total)
+#     total += 1
+# if total %2 != 0:
+#     print(total)
+
+
+numero = int(input("Digite um número: "))
+
+
+while numero >= 0:
+        if numero >= 0:
+                print(numero)
+                numero += int(input("Insira outro número: "))
+        else:
+                print("Número negativo")
+
